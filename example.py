e2cc9832b2844c6ac0df56c1dd9f4823e59b3553
William Baker <bakerw71@gmail.com>
2023-06-21 21:31:28 +0100

    Create example.py

---
+++ example.py
@@ -0,0 +1,18 @@
+#%%
+
+from utils.jax_helpers import JaxMemUsage
+JaxMemUsage.launch(interval=0.01)
+
+print(f'MaxMem: {JaxMemUsage.max_usage_str} Mem: {JaxMemUsage.usage_str}')
+
+#%%
+
+
+with tqdm(total=100, unit='batch') as tepoch:
+  for i in range(100):
+    # model.train()
+    
+    tepoch.set_postfix({'MaxMem': JaxMemUsage.max_usage_str, 'Mem': JaxMemUsage.usage_str})
+    tepoch.update(1)
+    
+  #%%
